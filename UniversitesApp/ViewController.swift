0d8e98102433f2d13bb5c13dfc1ad82c75f67689
Amr Magdy <amagdy195@yahoo.com>
2024-06-24 12:17:55 +0300

    Initial Commit

---
+++ UniversitesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniversitesApp
+//
+//  Created by Amr Magdy on 24/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
